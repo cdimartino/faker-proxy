a8bda539cdf7ef43dad6315753808b3c01e8a388
Chris DiMartino <vwchris@gmail.com>
2016-05-14 23:45:57 -0400

    It's Alive!

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+require 'json'
+
+require 'faker'
+
+set :show_exceptions, :after_handler
+
+get '/:mod/:method/?*?' do |mod, method, extra_params|
+  method = methodize(mod, method)
+  method.call(*to_params(extra_params)).to_json
+end
+
+error NameError do
+  [404, "I don't know nothin bout no #{env['sinatra.error'].message}"]
+end
+
+def to_params params_from_user
+  params_from_user.split('/').map do |param|
+    if param =~ /\A\d+\Z/
+      param.to_i
+    elsif %w{true false}.include?(param)
+      param == "true" # heh
+    else
+      param
+    end
+  end
+end
+
+def methodize mod, method
+  klass = Kernel.const_get("Faker::#{mod.capitalize}")
+  check_publicity(klass, method)
+  klass.public_method(method.to_sym)
+end
+
+def check_publicity klass, method
+  unless klass.public_methods(false).include?(method.to_sym)
+    raise NameError.new(
+      "#{klass}.#{method} is not allowed.  Must not be an inherited method.",
+      "#{klass}.#{method}"
+    )
+  end
+end
